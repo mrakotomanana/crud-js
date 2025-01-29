2b531daf288a23a5697cac3721cfdc871bee5068
mrakotomanana <rakotomanan@gmail.com>
2025-01-29 19:23:49 +0300

    change db mongo

--- database/db.js
+++ database/db.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb+srv://rakotomanan:xGv5HtU0cok1IFQU@cluster0.wetth.mongodb.net/todos?retryWrites=true&w=majority');
 
 // const db = mongoose.connection;
-// const db = mongoose.createConnection('mongodb+srv://rakotomanan:xGv5HtU0cok1IFQU@cluster0.wetth.mongodb.net/todos?retryWrites=true&w=majority', {
-const db = mongoose.createConnection('mongodb://127.0.0.1:27017/todos', {});
+const db = mongoose.createConnection('mongodb+srv://rakotomanan:xGv5HtU0cok1IFQU@cluster0.wetth.mongodb.net/todos?retryWrites=true&w=majority', {
+// const db = mongoose.createConnection('mongodb://127.0.0.1:27017/todos', {});
 
 
 db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB :'));
--- database/dbUser.js
+++ database/dbUser.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb+srv://rakotomanan:xGv5HtU0cok1IFQU@cluster0.wetth.mongodb.net/users?retryWrites=true&w=majority');
 
 // const db = mongoose.connection;
-// const db = mongoose.createConnection('mongodb+srv://rakotomanan:xGv5HtU0cok1IFQU@cluster0.wetth.mongodb.net/users?retryWrites=true&w=majority', {
-  const db = mongoose.createConnection('mongodb://127.0.0.1:27017/users', {});
+const db = mongoose.createConnection('mongodb+srv://rakotomanan:xGv5HtU0cok1IFQU@cluster0.wetth.mongodb.net/users?retryWrites=true&w=majority', {
+  // const db = mongoose.createConnection('mongodb://127.0.0.1:27017/users', {});
   
 db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB User :'));
 db.once('open', () => {
